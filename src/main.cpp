06c302b66793ed136c057b65747f80bfef149079
Colin Holzman-Klima <colinhk@icloud.com>
2022-07-17 15:20:05 -0700

    updated main driver

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include "../include/qtest.hpp"
 #include <iostream>
 
+QTestCase(Add, Positive) {
+    std::cout << "hello\n";
+}
+
 int main(int argc, char const *argv[]) {
-    std::cout << "out\n";
+    // qtest::QTestRunner::getInstance().registerTest();
+    QTestRegister(Add, Positive);
+    qtest::QTestRunner::getInstance().runTests();
+    std::cout << "haha\n";
     return 0;
 }
