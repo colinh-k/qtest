fb58026cf91826b9bd40ec9d624e2aae62a90842
Colin Holzman-Klima <colinhk@icloud.com>
2022-07-18 13:11:13 -0700

    update to new interface

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,24 @@
 #include <iostream>
 #include <exception>
 
+int add(int a, int b) {
+    return a + b;
+}
+
 QTestCase(Add, Positive) {
-    // QTEST_ASSERT_FALSE(false == true);
-    QTEST_EXPECT_THROWS(throw std::exception(), qtest::QTestException);
+    QTEST_EXPECT_THROWS(std::exception(), std::exception);
+    QTEST_EXPECT(add(1, 2) == 3);
+    QTEST_EXPECT(add(5, 5) == 10);
+}
+
+QTestCase(Add, Negative) {
+    QTEST_EXPECT(add(4, -4) == 0);
+    QTEST_EXPECT(add(-4, 4) != 0);
 }
 
 int main(int argc, char const *argv[]) {
     QTestRegister(Add, Positive);
+    QTestRegister(Add, Negative);
     QTestRunAll();
     return 0;
 }
