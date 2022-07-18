7cd0fe34dcc36574fe1fd112ec693c11477dae27
Colin Holzman-Klima <colinhk@icloud.com>
2022-07-18 13:11:06 -0700

    changed macros to expect statements; cleaned up test case macro

--- include/qtest.hpp
+++ include/qtest.hpp
@@ -4,38 +4,30 @@
 #include <iostream>
 #include <exception>
 
-#define _QTestCase(className)                          \
-    class className : public qtest::QTestBase {        \
-        public:                                        \
-            static className* getInstance (){          \
-                static className instance;             \
-                return &instance;                      \
-            }                                          \
-            className (className const&) = delete;     \
-            void operator=(className const&) = delete; \
-            ~className() = default;                    \
-            virtual void runTest() override;           \
-        private:                                       \
-            className() {}                             \
-    };                                                 \
-void className::runTest()
+#define QTEST_NAME(module_, testName) QTest_##module_##_##testName
+#define _QTestCase(module_, testName)                                                       \
+    class QTEST_NAME(module_, testName) : public qtest::QTestBase {                         \
+        public:                                                                             \
+            static QTEST_NAME(module_, testName)* getInstance (){                           \
+                static QTEST_NAME(module_, testName) instance(#module_, #testName);         \
+                return &instance;                                                           \
+            }                                                                               \
+            QTEST_NAME(module_, testName) (QTEST_NAME(module_, testName) const&) = delete;  \
+            void operator=(QTEST_NAME(module_, testName) const&) = delete;                  \
+            ~QTEST_NAME(module_, testName)() = default;                                     \
+            virtual void runTest() override;                                                \
+        private:                                                                            \
+            QTEST_NAME(module_, testName)(const std::string& _module,                       \
+                                          const std::string& _testName) :                   \
+                                          qtest::QTestBase(_module, _testName) {}           \
+    };                                                                                      \
+void QTEST_NAME(module_, testName)::runTest()
+#define QTestCase(module_, testName) _QTestCase(module_, testName)
 
-// #define QTestCase(module_, testName) QTEST_NAME(module_, testName)
-#define QTestCase(module_, testName) _QTestCase(QTest_##module_##_##testName)
-
-#define _QTestRegister(className) qtest::QTestRunner::getInstance().registerTest(className::getInstance())
-#define QTestRegister(module_, testName) _QTestRegister(QTest_##module_##_##testName)
+#define QTestRegister(module_, testName) qtest::QTestRunner::getInstance().registerTest(QTEST_NAME(module_, testName)::getInstance())
 #define QTestRunAll() qtest::QTestRunner::getInstance().runTests()
 
-// use two levels of indirection in order to
-// resolve __LINE__ correctly
-#define JOIN(x, y) x##y
-#define CONCAT(x,y) JOIN(x,y)
-// #define JOIN_SPECIAL(e, m) CONCAT(, __LINE__)
-// assertion macros
-// #define ASSERT_TRUE(exp)        qtest::qtestAssertEquals(exp, #exp, __FILE__, __LINE__)
-#define QTEST_EXPECT_TRUE(exp) qtest::expect(exp, true, #exp, __FILE__, __LINE__)
-#define QTEST_EXPECT_FALSE(exp)  qtest::expect(exp, false, #exp, __FILE__, __LINE__)
+#define QTEST_EXPECT(exp) qtest::Expector::expect(exp, "QTEST_EXPECT(" #exp ")", __FILE__, __LINE__)
 #define QTEST_EXPECT_THROWS(exp, exceptionType) \
 try {\
     exp;\
@@ -48,32 +40,37 @@ namespace qtest {
             QTestException(const std::string& _text,
                            const std::string& _file,
                            const std::size_t& _lineNumber) {
-                msg = _file + ":" + std::to_string(_lineNumber) +
-                       ": FAILED:\n    " + _text + "\n";
+                msg = "FAILED: " + _file + ":" + std::to_string(_lineNumber) +
+                       ":\n    " + _text + "\n";
             }
             const char* what() const noexcept override {
                 return msg.c_str();
             }
         private:
             std::string msg;
-            // std::string text;
-            // std::string file;
-            // std::size_t lineNumber;
     };
 
-    void expect(const bool& actual, const bool& expected,
-                const std::string text, const std::string file,
-                std::size_t lineNumber) {
-        if (actual == expected) return;  // ignore
-        throw QTestException(text, file, lineNumber);
-    }
+    struct Expector {
+        static void expect(const bool& actual,
+                    const std::string text,
+                    const std::string file,
+                    std::size_t lineNumber) {
+            if (actual) return;  // ignore if true
+            throw QTestException(text, file, lineNumber);
+        }
+    };
 
     class QTestBase {
         // base class for test cases
         public:
-            explicit QTestBase() {}
+            explicit QTestBase(const std::string& _module,
+                               const std::string& _testName) :
+                               name(_module + "." + _testName) {}
             virtual ~QTestBase() = default;
             virtual void runTest() = 0;
+            inline const std::string& getName() const { return name; }
+        private:
+            std::string name;
     };
 
     class QTestRunner {
@@ -93,27 +90,35 @@ namespace qtest {
 
             void runTests() {
                 std::uint32_t nPass = 0, nTotal = 0;
+                std::vector<QTestBase*> failedTests;
                 for (auto& test : qtests) {
+                    std::cout << "RUNNING " + test->getName() + "\n";
                     try {
                         test->runTest();
+                        std::cout << "PASSED\n";
                         nPass++;
                     } catch (QTestException& e) {
                         // test failed
                         std::cerr << e.what();
+                        failedTests.push_back(test);
                     } catch (std::exception& e) {
                         // uncaught exception, test marked as failed
-                        std::cerr << "Unexpected exception while running test:\n    "
+                        std::cerr << "FAILED: unexpected exception while running test:\n    "
                                   << e.what() << "\n";
+                        failedTests.push_back(test);
                     }
                     nTotal++;
                 }
 
                 if (nPass == nTotal) {
-                    std::cout << "Success: All tests passed ";
+                    std::cout << "\nSuccess: All tests passed ";
                 } else {
-                    std::cout << "Failure: Some tests failed ";
+                    std::cout << "\nFailure: Some tests failed (listed below) ";
                 }
                 std::cout << "[" + std::to_string(nPass) + "/" + std::to_string(nTotal) + "]\n";
+                for (QTestBase*& test : failedTests) {
+                    std::cout << "FAILED: " << test->getName() + "\n";
+                }
             }
 
         private:
