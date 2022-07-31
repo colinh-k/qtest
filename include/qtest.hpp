0141a5fc3826c202e1da22e417e904ba92d5f05e
Colin Holzman-Klima <colinhk@icloud.com>
2022-07-31 16:49:30 -0700

    added auto-test registration; changed results formatting a bit

--- include/qtest.hpp
+++ include/qtest.hpp
@@ -5,37 +5,43 @@
 #include <exception>
 
 // color macros for output
-#define RED    "\033[1m\033[31m"
-#define GREEN  "\033[1m\033[32m"
-#define YELLOW "\033[1m\033[33m"
-#define RESET  "\033[0m"
+// wrap string literals in these macros, or use underscore versions
+// for the raw code to handle non literal strings
+#define _RESET     "\033[0m"
+#define _RED       "\033[1m\033[31m"
+#define _GREEN     "\033[1m\033[32m"
+#define _YELLOW    "\033[1m\033[33m"
+#define RED(x)     _RED    x _RESET
+#define GREEN(x)   _GREEN  x _RESET
+#define YELLOW(x)  _YELLOW x _RESET
 
 #define QTEST_NAME(module_, testName) QTest_##module_##_##testName
-#define QTestCase(module_, testName)                                                        \
-    class QTEST_NAME(module_, testName) : public qtest::QTestBase {                         \
-        public:                                                                             \
-            static QTEST_NAME(module_, testName)* getInstance (){                           \
-                static QTEST_NAME(module_, testName) instance(#module_ "." #testName);      \
-                return &instance;                                                           \
-            }                                                                               \
-            QTEST_NAME(module_, testName) (QTEST_NAME(module_, testName) const&) = delete;  \
-            void operator=(QTEST_NAME(module_, testName) const&) = delete;                  \
-            ~QTEST_NAME(module_, testName)() = default;                                     \
-            virtual void runTest() override;                                                \
-        private:                                                                            \
-            QTEST_NAME(module_, testName)(const std::string& _name) :                       \
-                                          qtest::QTestBase(_name) {}                        \
-    };                                                                                      \
+#define QTEST_CASE(module_, testName)                                                 \
+class QTEST_NAME(module_, testName) : public qtest::QTestBase {                       \
+    public:                                                                           \
+        static QTEST_NAME(module_, testName)* getInstance () {                        \
+            static QTEST_NAME(module_, testName) instance(#module_ "." #testName);    \
+            return &instance;                                                         \
+        }                                                                             \
+        QTEST_NAME(module_, testName)(QTEST_NAME(module_, testName) const&) = delete; \
+        void operator=(QTEST_NAME(module_, testName) const&) = delete;                \
+        ~QTEST_NAME(module_, testName)() = default;                                   \
+        virtual void runTest() override;                                              \
+    private:                                                                          \
+        QTEST_NAME(module_, testName)(const std::string& _name) :                     \
+                                      qtest::QTestBase(_name) {}                      \
+        static bool registered;                                                       \
+};                                                                                    \
+bool QTEST_NAME(module_, testName)::registered =                                      \
+     qtest::QTestRunner::getInstance().registerTest(                                  \
+        QTEST_NAME(module_, testName)::getInstance()                                  \
+    );                                                                                \
 void QTEST_NAME(module_, testName)::runTest()
 
-#define QTestRegister(module_, testName)             \
-    qtest::QTestRunner::getInstance().registerTest(  \
-        QTEST_NAME(module_, testName)::getInstance() \
-    )
-#define QTestRunAll() qtest::QTestRunner::getInstance().runTests()
+#define QTEST_RUN_ALL() qtest::QTestRunner::getInstance().runTests()
 
-#define QTEST_EXPECT(exp)\
-    qtest::Expector::expect(exp, "QTEST_EXPECT(" #exp ")", __FILE__, __LINE__)
+#define QTEST_EXPECT(exp) \
+    qtest::Expector::expect(exp, #exp, __FILE__, __LINE__)
 
 // use a do-while statement so a ';' is required at the end
 // (for consistent style)
@@ -59,9 +65,9 @@ namespace qtest {
                            const std::string& _text,
                            const std::string& _file,
                            const std::size_t& _lineNumber) {
-                msg = RED "FAILED: " RESET + _file + ":" +
+                msg = RED("FAILED: ") + _file + ":" +
                       std::to_string(_lineNumber) +
-                      ": " + _msg + "\n    " + _text + "\n";
+                      ": " + _msg + "\n        " + _text + "\n";
             }
             const char* what() const noexcept override {
                 return msg.c_str();
@@ -104,8 +110,9 @@ namespace qtest {
             void operator=(QTestRunner const&) = delete;
             ~QTestRunner() = default;
 
-            void registerTest(QTestBase* test) {
+            bool registerTest(QTestBase* test) {
                 qtests.push_back(test);
+                return true;
             }
 
             void runTests();
@@ -120,10 +127,10 @@ namespace qtest {
         std::uint32_t nPass = 0, nTotal = 0;
         std::vector<QTestBase*> failedTests;
         for (auto& test : qtests) {
-            std::cout << YELLOW "RUNNING " RESET + test->getName() + "\n";
+            std::cout << YELLOW("RUNNING ") + test->getName() + "\n    ";
             try {
                 test->runTest();
-                std::cout << GREEN "PASSED\n" RESET;
+                std::cout << GREEN("PASSED\n");
                 nPass++;
             } catch (QTestException& e) {
                 // test failed
@@ -131,25 +138,25 @@ namespace qtest {
                 failedTests.push_back(test);
             } catch (std::exception& e) {
                 // uncaught exception, test marked as failed
-                std::cerr << RED "FAILED: " RESET \
-                          " unexpected exception while running test:\n    "
+                std::cerr << RED("FAILED:") \
+                          " unexpected exception while running test:\n        "
                             << e.what() << "\n";
                 failedTests.push_back(test);
             }
             nTotal++;
         }
 
+        std::cout << _YELLOW << "\n[" << std::to_string(nPass) << "/"
+                  << std::to_string(nTotal) << "] passing\n" << _RESET;
         if (nPass == nTotal) {
-            std::cout << "\n" GREEN "Success:" RESET \
-                      " All tests passed ";
+            std::cout << GREEN("SUCCESS:") \
+                      " All tests passed\n";
         } else {
-            std::cout << "\n" RED "Failure:" RESET \
-                      " Some tests failed (listed below) ";
+            std::cout << RED("FAILURE:") \
+                      " Some tests failed (listed below)\n";
         }
-        std::cout << "[" << std::to_string(nPass) << "/"
-                  << std::to_string(nTotal) << "]\n";
         for (QTestBase*& test : failedTests) {
-            std::cout << RED "FAILED: " RESET << test->getName() + "\n";
+            std::cout << RED("FAILED: ") << test->getName() + "\n";
         }
     }
 };  // namespace qtest
