a9b1da8cce9d3744de74b82108f85207cc41624d
Colin Holzman-Klima <colinhk@icloud.com>
2022-07-31 16:49:53 -0700

    added a third test case demonstrating testing with exceptions

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,22 @@ int add(int a, int b) {
     return a + b;
 }
 
-QTestCase(Add, Positive) {
-    QTEST_EXPECT_THROWS(std::exception(), std::exception);
+QTEST_CASE(Add, Positive) {
+    QTEST_EXPECT_THROWS(throw std::exception(), std::exception);
     QTEST_EXPECT(add(1, 2) == 3);
     QTEST_EXPECT(add(5, 5) == 10);
 }
 
-QTestCase(Add, Negative) {
+QTEST_CASE(Add, Negative) {
     QTEST_EXPECT(add(4, -4) == 0);
-    QTEST_EXPECT(add(-4, 4) != 0);
+    QTEST_EXPECT(add(-4, 4) == 0);
+}
+
+QTEST_CASE(Add, Throws) {
+    QTEST_EXPECT_THROWS(throw std::exception();, std::exception);
 }
 
 int main(int argc, char const *argv[]) {
-    QTestRegister(Add, Positive);
-    QTestRegister(Add, Negative);
-    QTestRunAll();
+    QTEST_RUN_ALL();
     return 0;
 }
