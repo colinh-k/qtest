35e45d69703e0aae6c82b61452a44064f400b0f1
Colin Holzman-Klima <colinhk@icloud.com>
2022-08-21 17:11:17 -0700

    demonstrates new expect_equals macro

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@ int add(int a, int b) {
 
 QTEST_CASE(Add, Positive) {
     QTEST_EXPECT_THROWS(throw std::exception(), std::exception);
+    printf("hello\n");
     QTEST_EXPECT(add(1, 2) == 3);
     QTEST_EXPECT(add(5, 5) == 10);
 }
@@ -15,10 +16,18 @@ QTEST_CASE(Add, Positive) {
 QTEST_CASE(Add, Negative) {
     QTEST_EXPECT(add(4, -4) == 0);
     QTEST_EXPECT(add(-4, 4) == 0);
+    QTEST_EXPECT_EQUALS(-56790, add(-42, -56748));
+    QTEST_EXPECT_EQUALS('c', 'c');
 }
 
 QTEST_CASE(Add, Throws) {
     QTEST_EXPECT_THROWS(throw std::exception();, std::exception);
+}
+
+QTEST_CASE(QTest, ExpectEquals) {
+    QTEST_EXPECT_EQUALS(42, 42);
+    QTEST_EXPECT_EQUALS('c', 'c');
+    QTEST_EXPECT_EQUALS(43, add(1, 41));
 }
 
 int main(int argc, char const *argv[]) {
